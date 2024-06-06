00706672626dd086d406c03e0b33be700b4fdc9f
Md Mujaheed Shahariar Riad <130202213+CopyProgrammer@users.noreply.github.com>
2024-06-06 16:02:36 +0600

    Add files via upload

---
+++ codeforces practice/Q_Digits.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+    int x,a,b,c,d,e,f,g,h,i,j;
+    scanf("%d", &x);
+
+    switch(x) {
+        case 10: scanf("%d", &j);
+        case 9: scanf("%d", &i);
+        case 8: scanf("%d", &h);
+        case 7: scanf("%d", &g);
+        case 6: scanf("%d", &f);
+        case 5: scanf("%d", &e);
+        case 4: scanf("%d", &d);
+        case 3: scanf("%d", &c);
+        case 2: scanf("%d", &b);
+        case 1: scanf("%d", &a);
+    }
+}
